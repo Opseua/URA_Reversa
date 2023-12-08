87cb99ecc89eec9752f7364795df26c69d2336d4
Orlando <opseua@gmail.com>
2023-12-08 06:15:52 -0300

    Novo ID da planilha e novo 'aut'

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ async function server(inf) {
         let infLog, retLog, infGoogleSheet, retGoogleSheet, err
 
         // DADOS GLOBAIS DA PLANILHA E FAZER O PARSE
-        gO.inf['id'] = '1UgKZbXFa_G3wn1XqVpfphWspSDt1EPrpzH0Trj-eMz4'; gO.inf['tab'] = 'ATENDIDO';
+        gO.inf['id'] = '1UzSX3jUbmGxVT4UbrVIB70na3jJ5qYhsypUeDQsXmjc'; gO.inf['tab'] = 'INDICAR_AUTOMATICO_[TELEIN]';
         let range = 'A2', id = gO.inf.id, tab = gO.inf.tab
         infGoogleSheet = {
             'action': 'get',
@@ -32,14 +32,21 @@ async function server(inf) {
             retGoogleSheet = retGoogleSheet.res[0][0]
         }
         gO.inf['json'] = JSON.parse(retGoogleSheet)
-        let colInf = inf && inf.col ? inf.col : gO.inf.json['col'];
-        let autInf = inf && inf.aut ? inf.aut : gO.inf.json['aut'];
+        let colInf = inf && inf.col ? inf.col : gO.inf.json['colUra'];
+        let autInf = inf && inf.aut ? inf.aut : gO.inf.json['autUra'];
         let conSplInf = inf && inf.conSpl ? inf.conSpl : gO.inf.json['conSpl'];
         let loginInf = inf && inf.login ? inf.login : gO.inf.json['login'];
         let passwordInf = inf && inf.password ? inf.password : gO.inf.json['password'];
         let interfaceInf = inf && inf.interface ? inf.interface : gO.inf.json['interface'];
         let id_interfaceInf = inf && inf.id_interface ? inf.id_interface : gO.inf.json['id_interface'];
         let subatualInf = inf && inf.subatual ? inf.subatual : gO.inf.json['subatual'];
+
+        for (let [index, value] of autInf.entries()) {
+            if (value.name == 'PHPSESSID') {
+                autInf = `PHPSESSID=${value.value}`
+                break
+            }
+        }
 
         let qtd = 0, stop = false
         while (!stop) {
@@ -72,88 +79,80 @@ async function server(inf) {
                     console.log(err);
                     infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retLeads }
                     retLog = await log(infLog);
-                    return retLeads
                 } else {
                     retLeads = retLeads.res
-                }
 
-                // SÓ RODAR SE O RETORNO DE leads FOR ARRAY
-                if (retLeads instanceof Array) {
-                    console.log(`${retLeads.length} LEADS COM O STATUS: '${infLeads.status}'`)
+                    // SÓ RODAR SE O RETORNO DE leads FOR ARRAY
+                    if (retLeads instanceof Array) {
+                        console.log(`${retLeads.length} LEADS COM O STATUS: '${infLeads.status}'`)
 
-                    // PEGAR INF | ALTERAR STATUS | MANDAR PARA PLANILHA
+                        // PEGAR INF | ALTERAR STATUS | MANDAR PARA PLANILHA
+                        for (let [index, value] of retLeads.entries()) {
+                            // ### PEGAR INF
+                            let infLeadGet, retLeadGet
+                            infLeadGet = { 'e': e, 'aut': autInf, 'leadId': value.leadId }
+                            retLeadGet = await leadGet(infLeadGet);
+                            if (!retLeadGet.ret) {
+                                err = `[server] FALSE: retLeadGet`
+                                console.log(err);
+                                infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retLeadGet }
+                                retLog = await log(infLog);
+                            } else {
+                                retLeadGet = retLeadGet.res
+                                // console.log(`LEAD ID: ${value.leadId} | TELEFONE: ${retLeadGet.tel}`)
 
-                    for (let [index, value] of retLeads.entries()) {
-                        // ### PEGAR INF
-                        let infLeadGet, retLeadGet
-                        infLeadGet = { 'e': e, 'aut': autInf, 'leadId': value.leadId }
-                        retLeadGet = await leadGet(infLeadGet);
-                        if (!retLeadGet.ret) {
-                            err = `[server] FALSE: retLeadGet`
-                            console.log(err);
-                            infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retLeadGet }
-                            retLog = await log(infLog);
-                            return retLeadGet
-                        } else {
-                            retLeadGet = retLeadGet.res
+                                // ###  ALTERAR STATUS
+                                let infLeadChangeStatus, retLeadChangeStatus
+                                infLeadChangeStatus = { 'e': e, 'aut': autInf, 'leadId': value.leadId, 'status': '1' } // '4' → Inapto | '1' → Venda Realizada
+                                retLeadChangeStatus = await leadChangeStatus(infLeadChangeStatus);
+                                if (!retLeadChangeStatus.ret) {
+                                    // if (!retLeadGet) { // →  TESTE
+                                    err = `[server] FALSE: retLeadChangeStatus`
+                                    console.log(err);
+                                    infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retLeadChangeStatus }
+                                    retLog = await log(infLog);
+                                } else {
+                                    retLeadChangeStatus = retLeadChangeStatus.res
+
+                                    // ### MANDAR PARA PLANILHA
+                                    time = dateHour().res
+                                    let sheetSend = [[
+                                        value.leadId, // LEAD ID
+                                        `${time.day}/${time.mon} ${time.hou}:${time.min}:${time.sec}`, // DATA DA CONSULTA
+                                        value.mailing,
+                                        value.date, // DATA URA
+                                        retLeadGet.cnpj,
+                                        retLeadGet.tel,
+                                        retLeadGet.administrador,
+                                        retLeadGet.email,
+                                        retLeadGet.razaoSocial,
+                                        `https://interface.telein.com.br/index.php?link=247&id_contato=${value.leadId}`
+                                    ]]
+                                    let sheetSendNew = sheetSend[0].join(conSplInf)
+
+                                    infGoogleSheet = {
+                                        'action': 'send',
+                                        'id': id,
+                                        'tab': tab,
+                                        'range': `${colInf}**`,
+                                        'values': [[`${sheetSendNew}`]]
+                                    }
+                                    retGoogleSheet = await googleSheet(infGoogleSheet);
+                                    if (!retGoogleSheet.ret) {
+                                        err = `[server] FALSE: retGoogleSheet`
+                                        console.log(err);
+                                        infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retGoogleSheet }
+                                        retLog = await log(infLog);
+                                        return retGoogleSheet
+                                    }
+                                    console.log(`[${(index + 1).toString().padStart(2, '0')}] ID: ${sheetSend[0][0]} | TEL: ${sheetSend[0][5]} | SHEET OK `)
+                                }
+                            }
                         }
-                        // console.log(`LEAD ID: ${value.leadId} | TELEFONE: ${retLeadGet.tel}`)
-
-                        // ###  ALTERAR STATUS
-                        let infLeadChangeStatus, retLeadChangeStatus
-                        infLeadChangeStatus = { 'e': e, 'aut': autInf, 'leadId': value.leadId, 'status': '1' } // '4' → Inapto | '1' → Venda Realizada
-                        retLeadChangeStatus = await leadChangeStatus(infLeadChangeStatus);
-                        if (!retLeadChangeStatus.ret) {
-                            err = `[server] FALSE: retLeadChangeStatus`
-                            console.log(err);
-                            infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retLeadChangeStatus }
-                            retLog = await log(infLog);
-                            return retLeadChangeStatus
-                        } else {
-                            retLeadChangeStatus = retLeadChangeStatus.res
-                        }
-                        // console.log(`LEAD ID: ${value.leadId} | STATUS ALTERADO PARA: ${infLeadChangeStatus.status}`)
-
-                        // ### MANDAR PARA PLANILHA
-                        time = dateHour().res
-                        let sheetSend = [[
-                            value.leadId, // LEAD ID
-                            `${time.day}/${time.mon} ${time.hou}:${time.min}:${time.sec}`, // DATA DA CONSULTA
-                            retLeadChangeStatus.status, // STATUS URA
-                            value.mailing,
-                            value.date, // DATA URA
-                            retLeadGet.cnpj,
-                            retLeadGet.razaoSocial,
-                            retLeadGet.tel,
-                            retLeadGet.email,
-                            retLeadGet.administrador,
-                            `https://interface.telein.com.br/index.php?link=247&id_contato=${value.leadId}`
-                        ]]
-                        let sheetSendNew = sheetSend[0].join(conSplInf)
-
-                        // console.log(sheetSendNew)
-                        // process.exit()
-
-                        infGoogleSheet = {
-                            'action': 'send',
-                            'id': id,
-                            'tab': tab,
-                            'range': `${colInf}**`,
-                            'values': [[`${sheetSendNew}`]]
-                        }
-                        retGoogleSheet = await googleSheet(infGoogleSheet);
-                        if (!retGoogleSheet.ret) {
-                            err = `[server] FALSE: retGoogleSheet`
-                            console.log(err);
-                            infLog = { 'e': e, 'folder': 'Registros', 'path': `${err}.txt`, 'text': retGoogleSheet }
-                            retLog = await log(infLog);
-                            return retGoogleSheet
-                        }
-                        console.log(`[${(index + 1).toString().padStart(2, '0')}] ID: ${sheetSend[0][0]} | NOVO STATUS: '${sheetSend[0][2]}' | TEL: ${sheetSend[0][7]} | SHEET OK `)
                     }
+                    ret['ret'] = true
+                    ret['msg'] = `SERVER: OK`
                 }
-                ret['ret'] = true
-                ret['msg'] = `SERVER: OK`
             } else {
                 console.log(`\n${time.day}/${time.mon} ${time.hou}:${time.min}:${time.sec} ## FORA DO DIA E HORÁRIO ##`)
             }
