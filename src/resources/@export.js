6e4a43ac5e5e7061832be0d37ecb3b2bbafd47c2
Orlando <opseua@gmail.com>
2024-04-08 02:15:30 -0300

    'getPathNew' → 'getPath'

--- src/resources/@export.js
+++ src/resources/@export.js
@@ -2,7 +2,7 @@
 await import('../../../Chrome_Extension/src/resources/@functions.js');
 
 // DEFINIR → LETTER | ROOT | FUNCTION | PROJECT | FILE | LINE
-let retGetPathNew = await getPathNew({ 'e': new Error(), 'isFunction': false, 'devSlave': cng == 1 ? 'CHROME' : 'URA_REVERSA' })
+let retGetPath = await getPath({ 'e': new Error(), 'isFunction': false, 'devSlave': cng == 1 ? 'CHROME' : 'URA_REVERSA' })
 
 // FUNÇÕES DESSE PROJETO
 await import('./leadChangeStatus.js')
