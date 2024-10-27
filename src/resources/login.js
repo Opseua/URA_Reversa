e42a90e514c117b64d7b1d9e970b01ab23930250
Orlando <opseua@gmail.com>
2024-10-27 09:36:34 -0300

    Padronização de 'return'

--- src/resources/login.js
+++ src/resources/login.js
@@ -75,7 +75,7 @@ async function login(inf) {
 
     } catch (catchErr) {
         let retRegexE = await regexE({ 'inf': inf, 'e': catchErr, }); ret['msg'] = retRegexE.res;
-    }; return { ...({ ret: ret.ret }), ...(ret.msg && { msg: ret.msg }), ...(ret.res && { res: ret.res }), };
+    }; return { ...({ 'ret': ret.ret }), ...(ret.msg && { 'msg': ret.msg }), ...(ret.res && { 'res': ret.res }), };
 };
 
 // CHROME | NODEJS
